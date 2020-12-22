d1e1839684bf980efac916e00a7379afe3a0b513
Abraham <qianyonglian@outlook.com>
2020-12-22 17:44:46 +0800

    add C#8 feature

--- IOSXAMac/ViewController.cs
+++ IOSXAMac/ViewController.cs
@@ -5,8 +5,45 @@ using UIKit;
 
 namespace IOSXAMac
 {
+    public enum Rainbow
+    {
+        Red,
+        Orange,
+        Yellow,
+        Green,
+        Blue,
+        Indigo,
+        Violet
+    }
+    
+    public class RGBColor
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+        public int C { get; set; }
+
+        public RGBColor(int a,int b,int c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+    }
     public partial class ViewController : UIViewController
     {
+        public static RGBColor FromRainbow(Rainbow colorBand) =>
+    colorBand switch
+    {
+        Rainbow.Red => new RGBColor(0xFF, 0x00, 0x00),
+        Rainbow.Orange => new RGBColor(0xFF, 0x7F, 0x00),
+        Rainbow.Yellow => new RGBColor(0xFF, 0xFF, 0x00),
+        Rainbow.Green => new RGBColor(0x00, 0xFF, 0x00),
+        Rainbow.Blue => new RGBColor(0x00, 0x00, 0xFF),
+        Rainbow.Indigo => new RGBColor(0x4B, 0x00, 0x82),
+        Rainbow.Violet => new RGBColor(0x94, 0x00, 0xD3),
+        _ => throw new ArgumentException(message: "invalid enum value", paramName: nameof(colorBand)),
+    };
+
         public ViewController(IntPtr handle) : base(handle)
         {
         }
@@ -20,7 +57,7 @@ namespace IOSXAMac
             TranslateButton.TouchUpInside += (object sender, EventArgs e) => {
 
                 Analytics.TrackEvent("First button is clicked at :" + DateTime.Now.ToLongTimeString());
-
+                Analytics.TrackEvent("Custom event name: " + FromRainbow(Rainbow.Indigo).A);
                 // Convert the phone number with text to a number
                 // using PhoneTranslator.cs
                 translatedNumber = PhoneTranslator.ToNumber(
